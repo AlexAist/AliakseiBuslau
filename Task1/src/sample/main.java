cbe703c8d898777348cadf5fee6db7bf36acc6ed
Alexey Buslov <36764868+AlexAist@users.noreply.github.com>
2018-12-10 06:48:48 +0300

    add Task1

---
+++ Task1/src/sample/main.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class main {
+    public static void main(String[] args) {
+        functions.testInt();
+        functions.testFloat();
+        functions.testDouble();
+        functions.testLong();
+        functions.testChar();
+        functions.testShort();
+        functions.testByte();
+        functions.testBoolean();
+    }
+}
